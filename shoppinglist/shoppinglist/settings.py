b68da2830d40c07f4871feb7fb0e4edb641141fb
lisetTablet <lizethbastidas@gmail.com>
2021-03-06 19:01:21 +0100

    change in settings.py

--- shoppinglist/shoppinglist/settings.py
+++ shoppinglist/shoppinglist/settings.py
@@ -93,8 +93,8 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("SQL_DATABASE", "sl_db")),
         "USER": os.environ.get("SQL_USER", "postgres"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
