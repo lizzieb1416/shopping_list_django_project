afdc6891c27c65474bb1293d350974f4a196df54
Ubuntu <ubuntu@ip-172-31-4-233.eu-west-3.compute.internal>
2021-03-06 21:40:21 +0000

    no postgres for my script :,(

--- shoppinglist/shoppinglist/settings.py
+++ shoppinglist/shoppinglist/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for shoppinglist project.
 
@@ -93,14 +94,15 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.environ.get("SQL_DATABASE", "sl_db"),
-        "USER": os.environ.get("SQL_USER", "postgres"),         
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"), 
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', str(BASE_DIR / 'db.sqlite3')),
+	'USER': os.environ.get('SQL_PASSWORD', 'postgres'),
+	'PASSWORD': os.environ.get('SQL_PASSWORD', 'postgres'),
+	'HOST': os.environ.get('SQL_HOST', 'localhost'),
+	'PORT': os.environ.get('SLQ_PORT', '5432'),
      }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
