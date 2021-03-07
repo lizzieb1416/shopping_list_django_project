6cce56dd58e0c832dd8ee40a05186bed3156f819
lisetTablet <lizethbastidas@gmail.com>
2021-03-07 13:09:25 +0100

    fdshhsdg

--- shoppinglist/shoppinglist/settings.py
+++ shoppinglist/shoppinglist/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"), 
-        "NAME": os.environ.get("SQL_DATABASE", "sl_db"),
+        "NAME": os.environ.get("SQL_DATABASE", "postgres"),
         "USER": os.environ.get("SQL_USER", "postgres"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "HOST": os.environ.get("SQL_HOST", "db"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
