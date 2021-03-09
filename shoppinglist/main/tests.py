f57e6bed7238875e3117de301eb1ca21b9775425
lisetTablet <lizethbastidas@gmail.com>
2021-03-09 16:17:42 +0100

    tests set up

--- shoppinglist/main/tests.py
+++ shoppinglist/main/tests.py
@@ -5,13 +5,11 @@ from django.test import TestCase
 class AnimalTestCase(TestCase):
     def setUp(self):
         pass
-        # Animal.objects.create(name="lion", sound="roar")
-        # Animal.objects.create(name="cat", sound="meow")
+        
 
     def test_animals_can_speak(self):
         self.assertTrue(True)
-        # """Animals that can speak are correctly identified"""
-        # lion = Animal.objects.get(name="lion")
-        # cat = Animal.objects.get(name="cat")
-        # self.assertEqual(lion.speak(), 'The lion says "roar"')
-        # self.assertEqual(cat.speak(), 'The cat says "meow"')
+        
+    def test_numbers(self):
+        self.assertEquals('foo'.upper(), 'FOO')
+        
